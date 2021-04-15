111180939b1a1bcfd8d34b85778341b853c3dba6
Mirko <mirkosbaglia2001@gmail.com>
2021-04-15 16:22:24 +0200

    Esercizio terminato

--- js/script.js
+++ js/script.js
@@ -1,24 +1,28 @@
 
 //GOAL: Attraverso l'apposita API di Boolean generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
 function boxGenerator(){
-  $.ajax({
-   url: 'https://flynn.boolean.careers/exercises/api/random/mail',
-   method: 'GET',
-   success: function(data) {
-     //console.log(data);
-     //console.log(data.response);
-     const res = data.response;
-     //console.log(res);
-     emails(res);
+  for (let i = 0; i <10; i++) {
+    $.ajax({
+     url: 'https://flynn.boolean.careers/exercises/api/random/mail',
+     method: 'GET',
+     success: function(data) {
+       //console.log(data);
+       //console.log(data.response);
+       const res = data.response;
+       //console.log(res);
+       emails(res);
 
-   },
+     },
 
-  })
+    })
+  }
 
 }
 
 function emails(email) {
   console.log(email);
+  const target = $('#target');
+  target.append('<li>' + email + '</li>')
 
 }
 
