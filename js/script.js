334aa4b242b83dbce7c836aec8ed5e84f90927b9
Mirko <mirkosbaglia2001@gmail.com>
2021-04-15 15:40:00 +0200

    Creazione pagine

---
+++ js/script.js
@@ -0,0 +1,19 @@
+
+//GOAL: Attraverso l'apposita API di Boolean generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+
+function initVue(){
+ new Vue({
+   el: '#app',
+   data : {
+
+  }
+
+ });
+}
+
+
+function init(){
+  initVue();
+
+}
+document.addEventListener('DOMContentLoaded', init);
